108652dab02f467ad5d8189f1c9f78e12842ccbd
yu-takahashi <36310653+yu-takahashi-314@users.noreply.github.com>
2024-11-02 19:11:45 +0900

    factorybot faker追加

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -6,5 +6,13 @@ RSpec.describe UsersController, type: :request do
       get users_path
       expect(response).to have_http_status(200)
     end
+
+    it "loads all of the users into @users" do
+      user1 = create(:user)
+      user2 = create(:user)
+      get users_path
+      expect(response.body).to include(user1.name)
+      expect(response.body).to include(user2.name)
+    end
   end
 end
