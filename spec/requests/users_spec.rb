7c3d14f94fdc3b3086977d6164aa47752ab215c2
yu-takahashi <36310653+yu-takahashi-314@users.noreply.github.com>
2024-11-02 19:04:12 +0900

    環境変数整理

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,22 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
+RSpec.describe UsersController, type: :request do
   describe "GET /users" do
     it "works! (now write some real specs)" do
-      get users_index_path
+      get users_path
       expect(response).to have_http_status(200)
-    end
-
-    it "renders the index template" do
-      get users_path
-      expect(response).to render_template(:index)
-    end
-
-    it "loads all of the users into @users" do
-      user1 = User.create!(name: "User One", email: "user1@example.com", password: "password")
-      user2 = User.create!(name: "User Two", email: "user2@example.com", password: "password")
-      get users_path
-      expect(assigns(:users)).to match_array([ user1, user2 ])
     end
   end
 end
